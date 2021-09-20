a55ff2eeb716a93d6d60bdf801d44b7b562e1c76
rmholsen <jaguar.kitty@gmail.com>
2021-09-20 14:32:00 -0400

    Completed

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,42 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [], }, action) {
+    // Start with state being an empty array of friends 
+    // reducer handles two actions: add and remove
+    switch(action.type) {
+        // CASE STATEMENTS HOW I MISSED YOU. OH SUCH FUN WE WILL HAVE TOGETHER CASE STATEMENTS
+
+        case "ADD_FRIEND": 
+            /* action = {
+                type: "ADD_FRIEND",
+                friend: {
+                    name: "Chrome Boi",
+                    hometown: "NYC",
+                    id: 1,
+                },
+                }; */
+            return({
+                ...state, // spread operator!
+                friends: [
+                    ...state.friends, 
+                    action.friend
+                ] // meaning you're adding the friend object in the action object to the friends array in the state
+                // and the spread operator includes all previous objects in the array
+                // I hope that was coherent
+            });
+            // don't forget there's no 'break' 
+
+        case "REMOVE_FRIEND":
+            const friend = state.friends.findIndex(friend => friend.id === action.id)
+            // finding the friend based on the id number in the action!
+            return({
+                ...state,
+                friends: [
+                  ...state.friends.slice(0, friend),
+                  ...state.friends.slice(friend + 1)
+                ]
+            })
+            
+        default: 
+            return state;
+            // always have a default thingie
+    };
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,22 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0,}, action){
+    // come to think of it I'm not entirely sure that comma has to be there? I guess it does?
+
+    switch(action.type) {
+        case "INCREASE":
+            return Object.assign({}, state, {
+                numberOfPresents: state.numberOfPresents + 1
+            });
+        
+            /* No, there is too much, let me sum up: 
+            "Object.assign(target, ...sources)" from Mdocs
+            The target object is what to apply things to
+            The sources is what to apply to the target object
+            And the form here specifically is referred to in Mdocs as cloning
+            Basically saying that we're cloning a state and turning it into state plus one
+
+            Now do the head jiggle.
+            */
+            default: 
+                return state;
+    }
 }
